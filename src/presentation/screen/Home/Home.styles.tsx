318dc0c3a20c1f7abc9fb08acd558a953d5fb78a
Richard Ben <frichardben@yahoo.com.br>
2023-03-05 19:32:42 -0300

    feat: added screen Home

---
+++ src/presentation/screen/Home/Home.styles.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	headerText: {
+		marginLeft: 8
+	},
+	headerUsername: {
+		color: '#fff',
+		fontSize: 14
+	},
+	headerPastDay: {
+		color: 'rgba(232, 230, 227, 0.5)',
+		fontSize: 12
+	}
+});
+
+export default styles;
---
+++ src/presentation/screen/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Avatar from '../../components/Avatar';
+import styles from './Home.styles';
+import Tag from '../../components/Tag';
+import Header from '../../components/Header';
+
+export const Home = () => {
+	return (
+		<Header>
+			<View style={styles.container}>
+				<Avatar />
+				<View style={styles.headerText}>
+					<Text style={styles.headerUsername}>hackerman</Text>
+					<Text style={styles.headerPastDay}>22 days ago</Text>
+				</View>
+			</View>
+			<View style={styles.container}>
+				<Tag />
+				<MaterialCommunityIcons
+					name="dots-horizontal"
+					size={24}
+					color="gray"
+					style={{ marginLeft: 6 }}
+				/>
+			</View>
+		</Header>
+	);
+};
