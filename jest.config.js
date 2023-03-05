c4603684f7ca0ea29e245b88af64261ac61e58ad
Richard Ben <frichardben@yahoo.com.br>
2023-03-05 17:06:17 -0300

    chore: added husky, lint-staged and commitlint

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-}
+	preset: 'jest-expo',
+	transformIgnorePatterns: [
+		'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+	],
+	setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+	modulePaths: ['<rootDir>/src/'],
+	testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
+};
