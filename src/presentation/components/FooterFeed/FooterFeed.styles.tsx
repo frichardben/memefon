94d113adfd0764b7ffdcdda3204d3bbbd9dda4a7
Richard Ben <frichardben@yahoo.com.br>
2023-03-05 20:18:10 -0300

    feat: added footerFeed

---
+++ src/presentation/components/FooterFeed/FooterFeed.styles.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+	footerFeedContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	footerFeedButton: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingRight: 16
+	},
+	footerFeedText: {
+		marginLeft: 4,
+		color: '#fff'
+	}
+});
+
+export default styles;
